da610f2061ff3fac6b4e64e633b7d29bfd99c887
Andrej Novoselstev Alexeevich <andrew.nvst@gmail.com>
2024-05-15 21:48:51 +0300

    Bottom Bar Implementation

---
+++ app/src/main/java/com/example/calc/BottomNavigationBar.kt
@@ -0,0 +1,56 @@
+package com.example.calc
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.calc.data.BottomNavigation
+
+val items = listOf(
+    BottomNavigation(
+        title = "Home",
+        icon = Icons.Rounded.Home
+    ),
+
+    BottomNavigation(
+        title = "Search",
+        icon = Icons.Rounded.Search
+    ),
+
+    BottomNavigation(
+        title = "Profile",
+        icon = Icons.Rounded.AccountCircle
+    )
+)
+
+@Composable
+fun BottomNavigationBar() {
+    NavigationBar() {
+        Row(
+            modifier = Modifier.background(MaterialTheme.colorScheme.inverseOnSurface)
+        ) {
+
+            items.forEachIndexed{ index, item ->
+                NavigationBarItem(
+                    selected = index == 0,
+                    onClick = { /*TODO*/ },
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.title,
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
